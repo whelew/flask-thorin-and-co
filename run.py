93f511002976ac2d1277350e9b8e3bfc02568f47
Lewis Wheeler <Lewisswheelerr@outlook.com>
2024-11-27 18:49:16 +0000

    Deployment to Heroku

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -17,6 +20,7 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
@@ -27,9 +31,14 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member = member)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, We have recieved your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
